87b21f62505cbbedc9c0dcf596ef11e0b4ad5c3a
Danielschnedier93 <s0562671@htw-berlin.de>
2019-08-27 19:54:55 +0200

    tested compass

--- app/src/main/java/com/example/firsttest/MainActivity.java
+++ app/src/main/java/com/example/firsttest/MainActivity.java
@@ -7,6 +7,10 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -15,6 +19,7 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -30,6 +35,7 @@ import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.math.Quaternion;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.ViewRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
@@ -47,7 +53,7 @@ import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     //Location
     static MainActivity instance;
@@ -57,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
 
     //Location Google API Request
     private Button requestButton;
+    private Button moveButton;
     public TextView results;
     public String LatLong;
     public double myLat;
@@ -90,6 +97,24 @@ public class MainActivity extends AppCompatActivity {
     //how many results do we want to show
     private final int maxResultCount = 3;
 
+    //create Nodes to hold the Planes
+    Node base = new Node();
+    Node model_1 = new Node();
+    Node model_2 = new Node();
+    Node model_3 = new Node();
+
+    //compass
+    private ImageView image;
+    int mAzimuth;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer, mMagnetometer;
+    float[] rMat = new float[9];
+    float[] orientation = new float[3];
+    private float[] mLastAccelerometer = new float[3];
+    private float[] mLastMagnetometer = new float[3];
+    private boolean mLastAccelerometerSet = false;
+    private boolean mLastMagnetometerSet = false;
+
 
     //Start Method ---------------------------------------------------------
     @Override
@@ -104,9 +129,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         instance = this;
 
+        //compass
+        image = findViewById(R.id.imageViewCompass);
+        // initialize android device sensor
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
         //find views
         txt_location = findViewById(R.id.location);
         requestButton = findViewById(R.id.sendRequest);
+        moveButton = findViewById(R.id.move);
         results = findViewById(R.id.results);
 
         //create arFragment here
@@ -132,6 +165,8 @@ public class MainActivity extends AppCompatActivity {
         FutureRenderer_1 = ViewRenderable.builder().setView(instance, plane_view_1).build();
         FutureRenderer_2 = ViewRenderable.builder().setView(instance, plane_view_2).build();
 
+        //shadows from planes look baaaaad
+        arFragment.getArSceneView().getPlaneRenderer().setShadowReceiver(false);
         //create tap listener
         arFragment.setOnTapArPlaneListener((HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
 
@@ -140,11 +175,6 @@ public class MainActivity extends AppCompatActivity {
             AnchorNode anchorNode = new AnchorNode(anchor);
             anchorNode.setParent(arFragment.getArSceneView().getScene());
 
-            //create Nodes to hold the Planes
-            Node base = new Node();
-            Node model_1 = new Node();
-            Node model_2 = new Node();
-            Node model_3 = new Node();
 
             // Fill the Nodes with Model and Position and add it to the anchor.
             model_1.setParent(base);
@@ -153,13 +183,15 @@ public class MainActivity extends AppCompatActivity {
 
             model_2.setParent(base);
             model_2.setRenderable(planeRenderable_1);
-            model_2.setLocalPosition(new Vector3(-1.0f, 0.0f, 0.0f));
+            model_2.setLocalPosition(new Vector3(-0.5f, 0.0f, 0.0f));
 
             model_3.setParent(base);
             model_3.setRenderable(planeRenderable_2);
-            model_3.setLocalPosition(new Vector3(1.0f, 0.0f, 0.0f));
+            model_3.setLocalPosition(new Vector3(0.5f, 0.0f, 0.0f));
 
             anchorNode.addChild(base);
+
+            rotateCorrectly();
         });
 
         //gps
@@ -192,6 +224,19 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
+        //test for live movement
+        moveButton.setOnClickListener(v -> {
+            model_3.setLocalRotation(Quaternion.axisAngle(new Vector3(1f, 0, 0), 90f));
+        });
+    }
+
+    public void rotateCorrectly() {
+        //TODO: get Kompass info
+        //calcualate direction
+        //turn planes in correct rotation and position
+
+
     }
 
     //--------------------------------------- GPS --------------------------------------------------
@@ -230,8 +275,56 @@ public class MainActivity extends AppCompatActivity {
         myLng = lng;
     }
 
+    //compass logic -------------------------------------------------------------------------------
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // for the system's orientation sensor registered listeners
+        mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_GAME);
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // to stop the listener and save battery
+        mSensorManager.unregisterListener(this, mAccelerometer);
+        mSensorManager.unregisterListener(this, mMagnetometer);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+            SensorManager.getRotationMatrixFromVector(rMat, event.values);
+            mAzimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0]) + 360) % 360;
+        }
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, mLastAccelerometer, 0, event.values.length);
+            mLastAccelerometerSet = true;
+        } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, mLastMagnetometer, 0, event.values.length);
+            mLastMagnetometerSet = true;
+        }
+        if (mLastAccelerometerSet && mLastMagnetometerSet) {
+            SensorManager.getRotationMatrix(rMat, null, mLastAccelerometer, mLastMagnetometer);
+            SensorManager.getOrientation(rMat, orientation);
+            mAzimuth = (int) (Math.toDegrees(SensorManager.getOrientation(rMat, orientation)[0]) + 360) % 360;
+        }
+
+        mAzimuth = Math.round(mAzimuth);
+        image.setRotation(-mAzimuth);
+        }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
 
     //own class for async task, to avoid NetworkOnMainThreadException
+    //api call, generating strings, gernerating plane models with correct Text on it
     private class GetJSONTask extends AsyncTask<String, Void, String> {
 
         @Override
@@ -260,16 +353,16 @@ public class MainActivity extends AppCompatActivity {
                     double dist = distance(MainActivity.instance.myLat, MainActivity.instance.myLng, placeLat, placeLng);
                     int distInMetersRounded = (int) (dist * 1000);
 
-                    String ResultString = "\n" + name + " - Distance: " + distInMetersRounded + " Meters";
+                    String ResultString = "\n" + name + "\n" + " Distance: " + distInMetersRounded + " Meters";
 
                     if (x == 1) {
-                        ResultString_1 = "\n" + name + " - Distance: " + distInMetersRounded + " Meters";
+                        ResultString_1 = "\n" + name + "\n" + " Distance: " + distInMetersRounded + " Meters";
                     }
                     if (x == 2) {
-                        ResultString_2 = "\n" + name + " - Distance: " + distInMetersRounded + " Meters";
+                        ResultString_2 = "\n" + name + "\n" + " Distance: " + distInMetersRounded + " Meters";
                     }
                     if (x == 3) {
-                        ResultString_3 = "\n" + name + " - Distance: " + distInMetersRounded + " Meters";
+                        ResultString_3 = "\n" + name + "\n" + " Distance: " + distInMetersRounded + " Meters";
                     }
 
                     resultString = resultString + ResultString;
@@ -351,7 +444,7 @@ public class MainActivity extends AppCompatActivity {
                 return temp[0];
             }
 
-          return null;
+            return null;
         }
 
         //calculate distance from two lat/lon points
@@ -374,7 +467,6 @@ public class MainActivity extends AppCompatActivity {
             return (rad * 180.0 / Math.PI);
         }
     }
-
 
     public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
